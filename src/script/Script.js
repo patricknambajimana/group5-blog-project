f25a7be3a6e380d16f4cbd8844e24e34fdc1822e
herve02 <twubaherve@gmail.com>
2025-07-17 13:08:39 +0200

    added no changes

--- src/script/Script.js
+++ src/script/Script.js
@@ -1,33 +1,27 @@
-const userName = document.getElementById("UserName");
 function loginUser(event) {
   event.preventDefault();
-  const username = document.getElementById("login-username").value.trim();
-  const password = document.getElementById("login-password").value.trim();
-  const msg = document.getElementById("login-msg");
+  const username = document.getElementById('login-username').value.trim();
+  const password = document.getElementById('login-password').value.trim();
+  const msg = document.getElementById('login-msg');
 
-  const users = JSON.parse(localStorage.getItem("users")) || [];
-  const foundUser = users.find(
-    (u) => u.username === username && u.password === password
-  );
+  const users = JSON.parse(localStorage.getItem('users')) || [];
+  const foundUser = users.find(u => u.username === username && u.password === password);
 
   if (!foundUser) {
-    msg.textContent = "Invalid credentials";
-    msg.style.color = "red";
+    msg.textContent = 'Invalid credentials';
+    msg.style.color = 'red';
     return;
   }
 
-  localStorage.setItem("loggedInUser", JSON.stringify(foundUser));
-  msg.textContent = "Login successful! Redirecting...";
-  msg.style.color = "green";
+  localStorage.setItem('loggedInUser', JSON.stringify(foundUser));
+  msg.textContent = 'Login successful! Redirecting...';
+  msg.style.color = 'green';
 
   setTimeout(() => {
-    if (foundUser.role === "admin") {
-       alert(foundUser.username)
-      window.location.href = "/src/pages/AdminBlog.html";
-     
-      // userName.innerHTML = foundUser.username;
+    if (foundUser.role === 'admin') {
+      window.location.href = 'AdminDashboard.html';
     } else {
-      window.location.href = "/src/pages/postBlog.html";
+      window.location.href = 'AuthorDashboard.html';
     }
   }, 1500);
 }
